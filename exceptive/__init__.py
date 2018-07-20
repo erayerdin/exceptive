050efb3659f7711ccb18f92531eb3319f2c152b5
Eray Erdin <eraygezer.94@gmail.com>
2018-07-20 20:57:10 +0300

    added setup

---
+++ exceptive/__init__.py
@@ -0,0 +1,10 @@
+"""
+Exceptive is a Python library that makes exception handling more programmatic and debuggable.
+"""
+
+__author__ = "Eray Erdin"
+__copyright__ = "Copyright 2018, Eray Erdin"
+__license__ = "Apache License 2.0"
+__maintainer__ = "Eray Erdin"
+__email__ = "eraygezer.94@gmail.com"
+__status__ = "Prototype"
---
+++ setup.py
@@ -0,0 +1,35 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open("README.md", "r") as f:
+    LONG_DESC = f.read()
+
+setup(
+    name="exceptive",
+    version="0.1.0a0",
+    description="Exceptive is a Python library that makes exception handling more programmatic and debuggable.",
+    long_description=LONG_DESC,
+    author="Eray Erdin",
+    author_email="eraygezer.94@gmail.com",
+    url="https://github.com/erayerdin/exceptive",
+    download_url="https://github.com/erayerdin/exceptive/archive/master.zip",
+    license="Apache License 2.0",
+    packages=["exceptive"],
+    include_package_data=True,
+    tests_require=["nose", "coverage"],
+    zip_safe=False,
+    test_suite="tests.runtests",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Testing"
+    ]
+)
