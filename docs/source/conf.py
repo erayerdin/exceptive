c3cc1cdb3da4d94f1e7dbcabb3d722cb6ced9fb9
Eray Erdin <eraygezer.94@gmail.com>
2018-07-22 18:38:51 +0300

    path reconfigured

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,8 +15,9 @@
 import os
 import sys
 # sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("../.."))
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__))))
+
 import exceptive
 
 # -- Project information -----------------------------------------------------
